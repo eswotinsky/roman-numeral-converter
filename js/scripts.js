b4da84f2891ad5bce2c1bd4e73134b83ea471a98
Eric Swotinsky and Tyler Wickline <eric+tyler@>
2018-01-18 14:27:35 -0800

    Add function for single character roman numerals and subtraction cases.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+var subRomanCases = /iv|ix|xl|xc|cd|cm/i;
 var nonRomanRegex = /[^IVXLCDM]/ig;
 
     $("form#roman-form").submit(function(event) {
         event.preventDefault();
-        var userInput = $("#romanInput").val();
+        var userInput = ($("#romanInput").val()).toUpperCase();
         if (nonRomanRegex.test(userInput)) {
             errorInvalidInput();
         }
@@ -17,4 +18,38 @@ var nonRomanRegex = /[^IVXLCDM]/ig;
 
 function errorInvalidInput() {
     $("#output").text("Please enter a valid roman numeral combination.")
+}
+
+
+
+function multipleRomanEval(userInput) {
+    for (var i = 0; i < userInput.length; i++) {
+        if (subRomanCases.test(userInput)) {
+
+        }
+    }
+}
+
+
+function singleRomanEval(userInput) {
+    switch (userInput) {
+    case 'I': return 1;
+    case 'V': return 5;
+    case 'X': return 10;
+    case 'L': return 50;
+    case 'C': return 100;
+    case 'D': return 500;
+    case 'M': return 1000;
+    default: return;
+    }
+}
+function subtractionSwitch(subInput) {
+    switch (subInput) {
+        case 'IV': return 4;
+        case 'IX': return 9;
+        case 'XL': return 40;
+        case 'XC': return 90;
+        case 'CD': return 400;
+        case 'CM': return 900;
+    }
 }
